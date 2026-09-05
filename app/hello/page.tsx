dd501e4f97818a7166d4a836da646000294910bc
kmarina38-crypto <kmarina38@gmail.com>
2026-09-05 17:00:28 +0900

    hello page 생성

---
+++ app/hello/page.tsx
@@ -0,0 +1,3 @@
+export default function HelloPage() {
+    return <h1>사랑합니다<br/>행복한가을만끽하세요</h1>;
+}
---
+++ app/meet/page.tsx
@@ -0,0 +1,3 @@
+export default function meetpage() {
+    return <h1>난경 크루즈 투어<br/>인생의 항해를 같이 떠나요<br/>마리나항해</h1>;
+}
